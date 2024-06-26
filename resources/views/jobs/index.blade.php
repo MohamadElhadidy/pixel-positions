482d23582315d7c0d72b2a6dc2e5e00b44d19e35
Mohamad Elhadidy <elhadidy384@gmail.com>
2024-06-26 13:00:22 +0300

    l

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -10,33 +10,26 @@
         <section class="mt-10">
             <x-section-heading>Top Jobs</x-section-heading>
             <div class="grid lg:grid-cols-3 gap-8 mt-6">
-                <x-job-card/>
-                <x-job-card/>
-                <x-job-card/>
+                @foreach($featured_jobs as $job)
+                    <x-job-card :$job/>
+                @endforeach
+
             </div>
         </section>
         <section>
             <x-section-heading>Tags</x-section-heading>
-            <div class="mt-6 space-x-1">
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-                <x-tag>Tag</x-tag>
-
+            <div class="mt-6 space-x-1 ">
+                @foreach($tags as $tag)
+                    <x-tag :$tag/>
+                @endforeach
             </div>
         </section>
         <section>
             <x-section-heading>Recent Jobs</x-section-heading>
             <div class="mt-6 space-y-6">
-                <x-job-card-wide/>
-                <x-job-card-wide/>
+                @foreach($jobs as $job)
+                    <x-job-card-wide :$job/>
+                @endforeach
             </div>
 
         </section>
